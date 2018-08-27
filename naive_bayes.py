4b58c1b1c1bf4eb510027838df409f8eb5dc1a39
Christian Limberg <climberg@tux-02.honda-ri.de>
2018-08-27 18:32:57 +0200

    naive bayes online classification

--- naive_bayes.py
+++ naive_bayes.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 
+from common.classification import get_numerical_label_encoders
 
 class NaiveBayes:
     def __init__(self):
@@ -23,45 +24,67 @@ class NaiveBayes:
         denom = (2 * pi * sigma) ** .5
         num = exp(-(point - mu) ** 2 / (2 * sigma))
         return num / denom
-    #train is just for calculating each mu and sigma
-    def train(self,x,y):
+    #fit is just for calculating each mu and sigma
+    def fit(self,x,y_names):
+        y, self.label_encoder_forward, self.label_encoder_backward = get_numerical_label_encoders(y_names)
         self.num_train_data = len(y)
         y_unique = set(y)
         self.num_classes = len(y_unique)
         self.mu = np.zeros((len(y_unique),x.shape[1]))
         self.sigma = np.zeros((len(y_unique),x.shape[1]))
         self.py = np.zeros((len(y_unique)))
+
+
+
         for label in y_unique:
             label_feats = x[y == label]
             self.mu[label] = np.mean(label_feats,axis=0)
             n = len(label_feats)
             self.py[label] = n
             v = ((label_feats - self.mu[label]) ** 2).mean(axis=0)
+            if len(label_feats) == 1:
+                v = 0.1
             self.sigma[label] = v
             #1d
             #self.sigma[label] = np.mean(np.linalg.norm([(self.mu[label] - iterx) for iterx in label_feats],axis=1)**2)
 
 
+    def fit_partial(self,x,y):
+        if not hasattr(self,'x_partial'):
+            self.x_partial = x
+            self.y_partial = y
+        else:
+            self.x_partial = np.vstack((self.x_partial,x))
+            self.y_partial = np.hstack((self.y_partial,y))
+        self.fit(self.x_partial,self.y_partial)
+
+
+
+
     def predict_proba(self,x):
+        if self.num_classes == 1:
+            raise Exception('only 1 class')
+
         y_pred = np.zeros((len(x),self.num_classes))
         for i,xiter in enumerate(x):
             normalize = 0
             for mu, sigma, py in zip(self.mu, self.sigma, self.py):
                 normalize += self.p(mu,sigma,xiter)*(py/self.num_train_data)
-            probs = []
-            for mu,sigma,py in zip(self.mu,self.sigma,self.py):
+            probs = np.zeros(self.num_classes)
+            for i2,(mu,sigma,py) in enumerate(zip(self.mu,self.sigma,self.py)):
                 likelihood = self.p(mu,sigma,xiter)
                 prior = py/self.num_train_data
-                probs.append((likelihood*prior)/normalize)
-            y_pred[i] = np.array(probs)
+                probs[i2] = (likelihood*prior)/normalize
+            y_pred[i] = probs
         return y_pred
 
     def predict(self,x):
         probas = self.predict_proba(x)
-        ret = np.argmax(probas,axis=1)
-        return ret
+        num_classes = np.argmax(probas,axis=1)
+        return np.array([self.label_encoder_backward[i] for i in num_classes])
 
-    def score(self,x,y):
+    def score(self,x,y_names):
+        y = np.array([self.label_encoder_forward[i] for i in y_names])
         y_pred = self.predict(x)
         return float(len(np.where(y==y_pred)[0]))/len(x)
 
@@ -72,7 +95,7 @@ if __name__ == '__main__':
                                          hypercube=True, shift=0.0, scale=1.0, shuffle=True, random_state=42)
     x_train, x_test, y_train, y_test = train_test_split( x, y, test_size = 0.33, random_state = 42)
     nb = NaiveBayes()
-    nb.train(x_train,y_train)
+    nb.fit(x_train,y_train)
 
 
     print(nb.predict(x_test))
@@ -86,5 +109,7 @@ if __name__ == '__main__':
         el = Ellipse(xy=m,width=2*sqrt(v[0]),height=2*sqrt(v[1]))
         ax.add_artist(el)
         el.set_alpha(0.3)
+
+    print('score', nb.score(x,y))
 
     plt.show()
